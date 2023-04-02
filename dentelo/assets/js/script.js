26be61be1dae619b8d53dbc85028198eff881c0b
Shravan <72138270+Shravan773@users.noreply.github.com>
2023-04-02 19:50:09 +0530

    Add files via upload

---
+++ dentelo/assets/js/script.js
@@ -0,0 +1,82 @@
+'use strict';
+
+
+
+/**
+ * addEvent on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+const options = {
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
+const openFormBtn = document.getElementById('open-form-btn');
+
+openFormBtn.addEventListener('click', function() {
+	window.open('book.html', 'User Data Form', 'width=600,height=500');
+});
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navbarToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  navbarToggler.classList.toggle("active");
+}
+
+addEventOnElem(navbarToggler, "click", toggleNav);
+
+const closeNav = function () {
+  navbar.classList.remove("active");
+  navbarToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNav);
+
+
+
+/**
+ * header active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
